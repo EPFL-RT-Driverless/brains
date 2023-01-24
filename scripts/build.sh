05ded83f48064fdd0d621c902496738a1f241236
Ted <oancea.tudor@icloud.com>
2023-01-24 16:51:58 +0100

    :bug: fixed shell scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,6 @@
 #  Copyright (c) 2023. Tudor Oancea EPFL Racing Team Driverless
 PYTHON_EXE=$(which python3)  # makes sure that the correct python interpreter is called in CMake
-PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources \  # colcon uses deprecated setuptools functions so we need to suppress warnings for it to successfully build the workspace
-  colcon build --symlink-install --cmake-args "-DPython3_EXECUTABLE=$PYTHON_EXE"
+PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources colcon build --symlink-install --cmake-args "-DPython3_EXECUTABLE=$PYTHON_EXE"
 # check current shell (bash or zsh) and source the apprioriate setup file
 if [ -n "$BASH_VERSION" ]; then
   source install/setup.bash
--- scripts/create_cpp_pkg.sh
+++ scripts/create_cpp_pkg.sh
@@ -2,6 +2,6 @@
 ros2 pkg create --build-type ament_cmake "$1" --dependencies \
   rclcpp action_msgs actionlib_msgs diagnostic_msgs geometry_msgs \
   lifecycle_msgs map_msgs nav_msgs pcl_msgs pendulum_msgs rosgraph_msgs \
-  sensor_msgs sensor_msgs_py shape_msgs statistics_msgs std_msgs stereo_msgs \
+  sensor_msgs shape_msgs statistics_msgs std_msgs stereo_msgs \
   tf2_geometry_msgs tf2_msgs tf2_sensor_msgs trajectory_msgs unique_identifier_msgs \
   visualization_msgs
