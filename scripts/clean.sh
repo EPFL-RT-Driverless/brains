fb3b621bb00153c25ea20b87e60e5c4ba13a66d3
Ted <oancea.tudor@icloud.com>
2023-02-17 16:58:24 +0100

    :sparkles: updated scripts to add checks

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,8 @@
 #  Copyright (c) 2023. Tudor Oancea EPFL Racing Team Driverless
+if [[ $(basename $PWD) != "brains" ]]; then
+    echo "Please run this script from the root of the brains repository"
+    exit 1
+fi
+
 PYTHONWARNINGS=ignore:::setuptools.command.install,ignore:::setuptools.command.easy_install,ignore:::pkg_resources \
   colcon build --cmake-target clean
