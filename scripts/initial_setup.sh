dec621b484507a18db74fc453d35821bc67d3352
Ted <oancea.tudor@icloud.com>
2023-02-24 13:51:19 +0100

    simplify build in initial_setup.sh

--- scripts/initial_setup.sh
+++ scripts/initial_setup.sh
@@ -7,8 +7,4 @@ fi
 pip3 install -e src/brains_python
 pre-commit install
 # build the workspace with colcon (use bash or zsh depending on the current shell)
-if [ -z "$ZSH_VERSION" ]; then
-    zsh scripts/build.sh
-else
-    bash scripts/build.sh
-fi
+./scripts/build.sh
