17e99ea982348ce1dab2ab48212f940fb6b85c01
Ted <oancea.tudor@icloud.com>
2023-03-10 12:53:25 +0100

    bruh

--- src/brains_python/brains_python/nodes/control_only.py
+++ src/brains_python/brains_python/nodes/control_only.py
@@ -12,18 +12,26 @@ from brains_python.control.ihm_acados import IHMAcadosParams, fsds_ihm_acados_pa
 from brains_python.control.stanley import StanleyParams, stanley_params_from_mission
 from brains_python.common import Mission
 import track_database as tdb
+from brains_custom_interfaces.srv import RestartFSDS, EnableApiFSDS
 
 
 class ControlOnly(Node):
     def __init__(self):
         super().__init__("control_only_node")
         track_name = self.declare_parameter("track_name", "fsds_competition_2")
-        self.car_state_sub = self.create_subscription(
-            CarState, "/fsds/car_state", self.callback, 10
-        )
-        self.car_controls_pub = self.create_publisher(
-            CarControls, "/fsds/car_controls", 10
-        )
+
+        restart_client = self.create_client(RestartFSDS, "/fsds/restart")
+        enable_api_client = self.create_client(EnableApiFSDS, "/fsds/enable_api")
+
+        while not restart_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("restart service not available, waiting again...")
+        while not enable_api_client.wait_for_service(timeout_sec=1.0):
+            self.get_logger().info("enable API service not available, waiting again...")
+        restart_client.call(RestartFSDS.Request())
+        self.get_logger().info("Restarted FSDS")
+        enable_api_client.call(EnableApiFSDS.Request(enabled=True))
+        self.get_logger().info("Enabled API")
+
         track = tdb.load_track(track_name.value)
         self.motion_planner_controller = MotionPlannerController(
             car_params=CarParams(**fsds_car_params),
@@ -42,6 +50,13 @@ class ControlOnly(Node):
             ),
         )
         self.last_control = np.zeros(2)
+
+        self.car_state_sub = self.create_subscription(
+            CarState, "/fsds/car_state", self.callback, 10
+        )
+        self.car_controls_pub = self.create_publisher(
+            CarControls, "/fsds/car_controls", 10
+        )
 
     def callback(self, car_state: CarState):
         control_result = self.motion_planner_controller.compute_control(
